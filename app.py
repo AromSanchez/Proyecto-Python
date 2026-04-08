0f105b12f228316668a4a6ebc5a1df81d9ffb2fd
Arom Patrik Sanchez Gutierrez <arom.sanchez@tecsup.edu.pe>
2026-04-08 11:05:09 -0500

    fix: correxxion de variables de conecxion

--- app.py
+++ app.py
@@ -1,42 +1,56 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask import jsonify
 import psycopg2
 import os
 
 app = Flask(__name__, template_folder='templates')
 
-# Configuración de la base de datos
 DB_HOST = 'dpg-d71v6phr0fns738t5c10-a.oregon-postgres.render.com'
 DB_NAME = 'test_xpec'
 DB_USER = 'test_xpec_user'
-DB_PASSWORD = 'iQyQwsfWRjaOQt4q6QKhF3RXWIAQOmkcd'
-
+DB_PASSWORD = 'iQyQwsfWRjaOQt4q6QKhF3RXWIAQOmkc'
 
 def conectar_db():
     try:
         conn = psycopg2.connect(
-            dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            sslmode='require'  # <- obligatorio en Render
+        )
         return conn
     except psycopg2.Error as e:
         print("Error al conectar a la base de datos:", e)
-
+        return None
 
 def crear_persona(dni, nombre, apellido, direccion, telefono):
     conn = conectar_db()
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO personas (dni, nombre, apellido, direccion, telefono) VALUES (%s, %s, %s, %s, %s)",
-                   (dni, nombre, apellido, direccion, telefono))
-    conn.commit()
-    conn.close()
+    if conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO personas (dni, nombre, apellido, direccion, telefono) VALUES (%s, %s, %s, %s, %s)",
+            (dni, nombre, apellido, direccion, telefono)
+        )
+        conn.commit()
+        conn.close()
 
 def obtener_registros():
-    conn = psycopg2.connect(
-        dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
-    cursor=conn.cursor()
-    cursor.execute("SELECT * FROM personas order by apellido")
-    registros = cursor.fetchall()
-    conn.close()
+    conn = conectar_db()
+    registros = []
+    if conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM personas ORDER BY apellido")
+        registros = cursor.fetchall()
+        conn.close()
     return registros
+
+def eliminar_por_dni(dni):
+    conn = conectar_db()
+    if conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM personas WHERE dni = %s", (dni,))
+        conn.commit()
+        conn.close()
 
 @app.route('/')
 def index():
@@ -50,25 +64,19 @@ def registrar():
     direccion = request.form['direccion']
     telefono = request.form['telefono']
     crear_persona(dni, nombre, apellido, direccion, telefono)
-    mensaje_confirmacion = "Registro Exitoso"
-    return redirect(url_for('index', mensaje_confirmacion=mensaje_confirmacion))
+    return redirect(url_for('index'))
 
 @app.route('/administrar')
 def administrar():
-    registros=obtener_registros()
-    return render_template('administrar.html',registros=registros)
+    registros = obtener_registros()
+    return render_template('administrar.html', registros=registros)
 
 @app.route('/eliminar/<dni>', methods=['POST'])
 def eliminar_registro(dni):
-    conn = psycopg2.connect(
-        dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
-    cursor=conn.cursor()
-    cursor.execute("DELETE FROM personas WHERE dni = %s", (dni,))
-    conn.commit()
-    conn.close()
+    eliminar_por_dni(dni)
     return redirect(url_for('administrar'))
 
+
 if __name__ == '__main__':
-    #Esto es nuevo
-    port = int(os.environ.get('PORT',5000))    
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
